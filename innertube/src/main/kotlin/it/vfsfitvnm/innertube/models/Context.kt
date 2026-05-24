2a99fa3ed467dc68abd6c2cbc2a6848c8b019c48
शिवराज <162401954+shivsagarsingh265@users.noreply.github.com>
2026-05-25 01:32:03 +0530

    Update Context.kt

--- innertube/src/main/kotlin/it/vfsfitvnm/innertube/models/Context.kt
+++ innertube/src/main/kotlin/it/vfsfitvnm/innertube/models/Context.kt
@@ -37,8 +37,6 @@ data class Context(
                 clientName = "ANDROID_MUSIC",
                 clientVersion = "6.41.51",
                 platform = "MOBILE",
-                osName = "Android",
-                osVersion = "14",
                 androidSdkVersion = 34,
                 userAgent = "com.google.android.apps.youtube.music/6.41.51 (Linux; U; Android 14; Build/UQ1A.240205.002) Max/4.4.1"
             )
